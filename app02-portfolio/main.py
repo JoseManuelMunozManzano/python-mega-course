32df58b26c0931af44208d08838eaf595431c92e
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-11-26 06:05:58 +0100

    Create columns, image and description

---
+++ app02-portfolio/main.py
@@ -0,0 +1,19 @@
+# streamlit run main.py
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.jpeg")
+
+with col2:
+    st.title("José Manuel Muñoz Manzano")
+    content = """
+    Hi, I am Jose Manuel! I am a RPG, Java, JavaScript and Python developer. 
+I have experience with DB2 and MySql databases.
+I have worked with companies such as ATOS and A.M.A. and currently I'm working in Diners Club.
+I really love to code!
+    """
+    st.info(content)
