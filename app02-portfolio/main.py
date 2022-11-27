0db620f6a337a5dce828692ba132b71e86aa7e94
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-11-27 06:47:44 +0100

    Add description, image and links

--- app02-portfolio/main.py
+++ app02-portfolio/main.py
@@ -24,13 +24,19 @@ Below you can find some of the apps I have build in Python. Feel free to contact
 """
 st.text(content2)
 
-col3, col4 = st.columns(2)
+# Se pasa la ratio dimension de las columnas. La primera columna será 3 veces más ancha que la columna del medio.
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pandas.read_csv("data.csv", sep=";")
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
