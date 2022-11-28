9d3e57d93c76cb76d5015b09580afb71a87a799c
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-11-28 19:43:54 +0100

    Página Contact_Us y mandar emails

--- student-project-01-company-website/pages/Contact_Us.py
+++ student-project-01-company-website/pages/Contact_Us.py
@@ -1,15 +1,18 @@
 import streamlit as st
+import pandas
 from send_email import send_email
 
-st.header("Contact Me")
+df = pandas.read_csv("topics.csv")
 
-with st.form(key="email_forms"):
-    user_email = st.text_input("Your email address")
-    raw_message = st.text_area("Your message")
+with st.form(key="email_form"):
+    user_email = st.text_input("Your Email Address")
+    topic = st.selectbox("What topic do you cant to discuss?", df["topic"])
+    raw_message = st.text_area("Text")
     message = f"""\
 Subject: New email from {user_email}
 
 From: {user_email}
+Topic {topic}
 {raw_message}
 """
     button = st.form_submit_button("Submit")
