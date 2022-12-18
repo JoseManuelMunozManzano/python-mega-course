b7bdd31eaa84b022d0be1daeea12a64d009c284f
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-12-18 05:42:36 +0100

    Retrieve filtered temperature/sky data from OpenWeather

--- app07-weather-forecast-data-app/backend.py
+++ app07-weather-forecast-data-app/backend.py
@@ -10,8 +10,16 @@ def get_data(place, forecast_dasy=None, kind=None):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
-    return data
+    filtered_data = data["list"]
+    # Obtenemos 8 valores cada 24 horas. Como el máximo son 5 días tenemos como mucho 40 valores.
+    nr_values = 8 * forecast_dasy
+    filtered_data = filtered_data[:nr_values]
+    if kind == "Temperature":
+        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
+    if kind == "Sky":
+        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
+    return filtered_data
 
 
 if __name__ == "__main__":
-    print(get_data(place="Madrid"))
+    print(get_data(place="Madrid", forecast_dasy=3, kind="Temperature"))
